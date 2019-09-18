844a60b952f262cd4a579d6e718a0378a9c1a8f0
Shota Kwasaki <shotan.0924@icloud.com>
2019-09-18 15:15:27 +0900

    課題1.1.2「データ型（コレクション）」

--- 1.1.2/1.1.2/ViewController.swift
+++ 1.1.2/1.1.2/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var stringOne:[String] = ["掃除","洗濯","皿洗い","ゴミ捨て"]
+        var numberThreeToSix:[[Int]] = [[3,4],[5,6]]
+        
+        var dictionaryOne:[String:Int] = ["掃除":2,"洗濯":9,"皿洗い":7,"ゴミ捨て":8]
+        
+        print(stringOne[2])
+        print(numberThreeToSix[0])
+        print(dictionaryOne["掃除"] as Any)
     }
 
 
 }
+
+
 
