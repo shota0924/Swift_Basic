420863402e0182c5785096f5cf84659ea26446bc
Shota Kwasaki <shotan.0924@icloud.com>
2019-09-18 14:47:29 +0900

    課題1.1.1「データ型（プリミティブ）」

---
+++ 1.1.1/1.1.1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  1.1.1
+//
+//  Created by Shota on 2019/09/18.
+//  Copyright © 2019 Shota Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let numberOne:Int = 1
+        let greeting:String = "こんにちは"
+        let numberTwo:Double = 2.0
+        let a:Bool = true && true
+        
+        print(numberOne)
+        print(greeting)
+        print(numberTwo)
+        print(a)
+    }
+
+
+}
+
