c23467315d4dcd6c9dec392e8b5656acc577fa61
Shota Kwasaki <shotan.0924@icloud.com>
2019-10-02 13:15:41 +0900

    課題1.1.4「クラス・プロパティ」
    git commit -a -m 課題1.1.4「クラス・プロパティ」

--- 1.1.4/1.1.4/ViewController.swift
+++ 1.1.4/1.1.4/ViewController.swift
@@ -15,10 +15,44 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         class Account{
-            name 
-        }
-    }
-
-
+            var name:String
+            var age:Int
+            var sex:String
+            var favoriteLanguage:String
+            
+            
+            init() {
+                self.name = "名無し"
+                self.age = 0
+                self.sex = "不明"
+                self.favoriteLanguage = "無し"
+            }
+            
+            func intoroduction()  {
+                print(name+favoriteLanguage+String(age)+"です。")
+        
+            }
+            
+}
+        let hanako = Account()
+        hanako.name = "山田花子さんは"
+        hanako.age = 20
+        hanako.sex = "女性"
+        hanako.favoriteLanguage = "javaが好きな"
+        hanako.intoroduction()
+        
+        let taro = Account()
+        taro.name = "佐藤太郎くんは"
+        taro.age = 20
+        taro.sex = "男の子"
+        taro.favoriteLanguage = "Swiftが好きな"
+        taro.intoroduction()
+        
+        
+        
+        
+        
+        
+}
 }
 
