2ec0aa5206f97ec39103b7624801e9ca6be988eb
Shota Kwasaki <shotan.0924@icloud.com>
2019-10-02 15:29:19 +0900

    課題1.1.4「クラス・プロパティ・メソッド」

--- 1.1.4/1.1.4/ViewController.swift
+++ 1.1.4/1.1.4/ViewController.swift
@@ -9,50 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        class Account{
-            var name:String
-            var age:Int
-            var sex:String
-            var favoriteLanguage:String
-            
-            
-            init() {
-                self.name = "名無し"
-                self.age = 0
-                self.sex = "不明"
-                self.favoriteLanguage = "無し"
-            }
-            
-            func intoroduction()  {
-                print(name+favoriteLanguage+String(age)+"です。")
+        let hanako = Account(name: "山田花子", age: 20, sex: "女性", FavoriteLanguage: "Java")
         
-            }
-            
-}
-        let hanako = Account()
-        hanako.name = "山田花子さんは"
-        hanako.age = 20
-        hanako.sex = "女性"
-        hanako.favoriteLanguage = "javaが好きな"
         hanako.intoroduction()
         
-        let taro = Account()
-        taro.name = "佐藤太郎くんは"
-        taro.age = 20
-        taro.sex = "男の子"
-        taro.favoriteLanguage = "Swiftが好きな"
+        let taro = Account(name: "佐藤太郎", age: 20, sex: "男性", FavoriteLanguage:"Swift")
+        
         taro.intoroduction()
         
-        
-        
-        
-        
-        
-}
+    }
 }
 
