99c0c843f1a706d679c224a896c581fae3a16843
川崎翔太 <toyo0924kawa@gmail.com>
2019-12-04 15:02:19 +0900

    課題2.1.18[画面遷移（UINavigationController）]が完了しています。

---
+++ 2.1.18/2.1.18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.3.2
+//
+//  Created by Shota on 2019/12/03.
+//  Copyright © 2019 Shota Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
