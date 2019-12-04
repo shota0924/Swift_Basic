f75b3ca2af9365db8bc7c36c4eb49932098fb1d7
川崎翔太 <toyo0924kawa@gmail.com>
2019-12-04 14:53:56 +0900

    課題2.1.12[UICollectionView] 未完成　Collection viewが表示されない。

---
+++ 2.1.12/2.1.12/CollectionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionViewController.swift
+//  2.1.12
+//
+//  Created by Shota on 2019/12/03.
+//  Copyright © 2019 Shota Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "cell"
+
+class CollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 100
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+}
---
+++ 2.1.12/2.1.12/ViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ViewController.swift
+//  2.1.12
+//
+//  Created by Shota on 2019/12/03.
+//  Copyright © 2019 Shota Kawasaki. All rights reserved.
+//
+
+import UIKit
+
